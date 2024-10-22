4f4d6a5e04f20453cc0a208b77017628955e73f1
Diogo Lara <diogo.lara.dev@gmail.com>
2024-10-22 19:22:31 -0300

    chore: update and fix database models
    organize tables and relations

--- prisma/migrations/20241022222113_add_products_categories_stocks_movements_and_relations/migration.sql
+++ prisma/migrations/20241022222113_add_products_categories_stocks_movements_and_relations/migration.sql
@@ -34,6 +34,18 @@ CREATE TABLE "categories_to_products" (
 );
 
 -- CreateTable
+CREATE TABLE "stocks" (
+    "id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "stocks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "stock_movements" (
     "id" TEXT NOT NULL,
     "productId" TEXT NOT NULL,
@@ -45,11 +57,17 @@ CREATE TABLE "stock_movements" (
     CONSTRAINT "stock_movements_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "stocks_product_id_key" ON "stocks"("product_id");
+
 -- AddForeignKey
 ALTER TABLE "categories_to_products" ADD CONSTRAINT "categories_to_products_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "categories_to_products" ADD CONSTRAINT "categories_to_products_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "stocks" ADD CONSTRAINT "stocks_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "stock_movements" ADD CONSTRAINT "stock_movements_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
